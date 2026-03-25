406e3c344e005a582f4568894ea687d2221995ec
Devarinti Sai Rupesh <devarintisairupesh840@gmail.com>
2026-03-25 12:45:57 +0530

    Update interviewPage.jsx

--- client/src/pages/interviewPage.jsx
+++ client/src/pages/interviewPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import Step1SetUp from '../components/Step1SetUp'
-import Step2Interview from '../components/Step2Interview'
+import Step2Interview from '../components/Step2interview'
 import Step3Report from '../components/Step3Report'
 
 function InterviewPage() {
